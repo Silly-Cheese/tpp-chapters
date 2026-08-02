157eeb1d4c12b5c6d29376ddeb84c0d0fafcda3b
Executive_Eagle <christophershelley257@gmail.com>
2026-08-02 14:38:08 -0500

    Connect Firebase production services

---
+++ assets/js/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-storage.js";
+
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyDtn5Tt5syKJm9gQi-tQRGYLv0_636C13Y",
+  authDomain: "tpp-chapters.firebaseapp.com",
+  projectId: "tpp-chapters",
+  storageBucket: "tpp-chapters.firebasestorage.app",
+  messagingSenderId: "962501948087",
+  appId: "1:962501948087:web:d145a48db8bac5f3d41332"
+});
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
+
+export const authPersistenceReady = setPersistence(auth, browserLocalPersistence)
+  .catch((error) => {
+    console.error("Unable to set Firebase Auth persistence.", error);
+  });
